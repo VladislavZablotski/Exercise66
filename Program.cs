480c29633640ae8034c6969c0dd9a3dbd0101efc
unknown <zab_vl@tutby>
2023-04-11 19:26:15 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число M");
+int M =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N =int.Parse(Console.ReadLine());
+int temp;
+
+if (M>N) 
+{
+    temp = N;
+    N = M;
+    M = temp;
+}
+temp = M;
+
+int Sum()
+{
+if(M>=N)
+{
+    return temp;
+}
+M++;
+return M + Sum();
+}
+
+Console.WriteLine($"Сумма чисел равна {Sum()}");
